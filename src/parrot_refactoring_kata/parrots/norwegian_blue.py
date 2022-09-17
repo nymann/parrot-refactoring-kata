5b926818811726da9f732f6ef68fb1119b718bb1
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-09-17 14:00:09 +0200

    :recycle: Remove unnecessary list comprehension

--- src/parrot_refactoring_kata/parrots/norwegian_blue.py
+++ src/parrot_refactoring_kata/parrots/norwegian_blue.py
@@ -5,4 +5,4 @@ class NorwegianBlueParrot(Parrot):
     def speed(self) -> float:
         if self._nailed:
             return 0
-        return min([24.0, self._voltage * self._base_speed()])
+        return min(24.0, self._voltage * self._base_speed())
