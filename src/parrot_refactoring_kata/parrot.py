2c7d7d2005c6ef4b42f9b12fb166ba9e97a676a5
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-09-17 02:16:32 +0200

    :white_check_mark: Parameterize tests

--- src/parrot_refactoring_kata/parrot.py
+++ src/parrot_refactoring_kata/parrot.py
@@ -8,30 +8,38 @@ class ParrotType(Enum):  # If it is not available, just remove it.
 
 
 class Parrot:
-    def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
+    def __init__(
+        self,
+        type_of_parrot: ParrotType,
+        number_of_coconuts: int,
+        voltage: int | float,
+        nailed: bool,
+    ) -> None:
         self._type = type_of_parrot
         self._number_of_coconuts = number_of_coconuts
         self._voltage = voltage
         self._nailed = nailed
 
-    def speed(self):
+    def speed(self) -> float:
         if self._type == ParrotType.EUROPEAN:
             return self._base_speed()
         if self._type == ParrotType.AFRICAN:
-            return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
+            return max(
+                0,
+                self._base_speed() - self._load_factor() * self._number_of_coconuts,
+            )
         if self._type == ParrotType.NORWEGIAN_BLUE:
             if self._nailed:
                 return 0
-            else:
-                return self._compute_base_speed_for_voltage(self._voltage)
+            return self._compute_base_speed_for_voltage(self._voltage)
 
         raise ValueError("should be unreachable")
 
-    def _compute_base_speed_for_voltage(self, voltage):
+    def _compute_base_speed_for_voltage(self, voltage) -> float:
         return min([24.0, voltage * self._base_speed()])
 
-    def _load_factor(self):
-        return 9.0
+    def _load_factor(self) -> float:
+        return 9.0  # noqa: WPS432 magic-number
 
-    def _base_speed(self):
-        return 12.0
+    def _base_speed(self) -> float:
+        return 12.0  # noqa: WPS432 magic-number
--- tests/unit_tests/test_parrot.py
+++ tests/unit_tests/test_parrot.py
@@ -1,37 +1,19 @@
+import pytest
+
 from parrot_refactoring_kata.parrot import Parrot
 from parrot_refactoring_kata.parrot import ParrotType
 
-
-def test_speedOfEuropeanParrot():
-    parrot = Parrot(ParrotType.EUROPEAN, 0, 0, False)
-    assert parrot.speed() == 12.0
-
-
-def test_speedOfAfricanParrot_With_One_Coconut():
-    parrot = Parrot(ParrotType.AFRICAN, 1, 0, False)
-    assert parrot.speed() == 3.0
-
-
-def test_speedOfAfricanParrot_With_Two_Coconuts():
-    parrot = Parrot(ParrotType.AFRICAN, 2, 0, False)
-    assert parrot.speed() == 0.0
+test_cases = [
+    (Parrot(type_of_parrot=ParrotType.EUROPEAN, number_of_coconuts=0, voltage=0, nailed=False), 12.0),
+    (Parrot(type_of_parrot=ParrotType.AFRICAN, number_of_coconuts=1, voltage=0, nailed=False), 3.0),
+    (Parrot(type_of_parrot=ParrotType.AFRICAN, number_of_coconuts=2, voltage=0, nailed=False), 0),
+    (Parrot(type_of_parrot=ParrotType.AFRICAN, number_of_coconuts=0, voltage=0, nailed=False), 12.0),
+    (Parrot(type_of_parrot=ParrotType.NORWEGIAN_BLUE, number_of_coconuts=0, voltage=1.5, nailed=True), 0),
+    (Parrot(type_of_parrot=ParrotType.NORWEGIAN_BLUE, number_of_coconuts=0, voltage=1.5, nailed=False), 18.0),
+    (Parrot(type_of_parrot=ParrotType.NORWEGIAN_BLUE, number_of_coconuts=0, voltage=4, nailed=False), 24.0),
+]
 
 
-def test_speedOfAfricanParrot_With_No_Coconuts():
-    parrot = Parrot(ParrotType.AFRICAN, 0, 0, False)
-    assert parrot.speed() == 12.0
-
-
-def test_speedNorwegianBlueParrot_nailed():
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 1.5, True)
-    assert parrot.speed() == 0.0
-
-
-def test_speedNorwegianBlueParrot_not_nailed():
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 1.5, False)
-    assert parrot.speed() == 18.0
-
-
-def test_speedNorwegianBlueParrot_not_nailed_high_voltage():
-    parrot = Parrot(ParrotType.NORWEGIAN_BLUE, 0, 4, False)
-    assert parrot.speed() == 24.0
+@pytest.mark.parametrize("parrot,expected_speed", test_cases)
+def test_speed_of_parrot(parrot: Parrot, expected_speed: float) -> None:
+    assert parrot.speed() == expected_speed
